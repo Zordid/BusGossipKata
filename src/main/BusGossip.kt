5ed74b726c85304814692891a5d34980c5ffedf5
Olaf Gottschalk <Olaf.Gottschalk@bmwgroup.com>
2017-12-01 21:14:11 +0100

    Reduce many constructs to more Kotlin like code
    Improvements in code on either Kotlin coding style and also algorithmic
    approach. For example to test whether every driver knows every gossip,
    it is enough to ask whether every driver knows exactly as many gossips
    as there are drivers - compared to formerly testing for equality of the
    sets of gossip.

--- src/main/BusGossip.kt
+++ src/main/BusGossip.kt
@@ -10,21 +10,13 @@ data class Driver(val name: String,
                   private val route: List<Int>,
                   private val gossips: Set<Gossip> = setOf(Gossip("$name's gossip"))) {
 
-    val knowsAbout = mutableSetOf<Gossip>()
+    val knowsAbout = gossips.toMutableSet()
     val whereAreYou: Int
         get() = route[index % route.size]
 
     private var index = 0
 
-    init {
-        knowsAbout.addAll(gossips)
-    }
-
-    fun haveYouHeardAboutThis(gossips: Collection<Gossip>): Boolean {
-        if (knowsAbout.containsAll(gossips)) return true
-        knowsAbout.addAll(gossips)
-        return false
-    }
+    fun haveYouHeardAboutThis(gossips: Collection<Gossip>) = !knowsAbout.addAll(gossips)
 
     fun drive() {
         index++
@@ -48,12 +40,6 @@ class Simulation(private val drivers: Collection<Driver>) {
         }
     }
 
-    private val everyGossipInTheWorld: Set<Gossip> by lazy {
-        val gossip = mutableSetOf<Gossip>()
-        drivers.forEach { gossip.addAll(it.knowsAbout) }
-        gossip
-    }
-
     fun run(workingHours: Int = 8): Int {
 
         for (i in 0 until workingHours * 60) {
@@ -72,18 +58,10 @@ class Simulation(private val drivers: Collection<Driver>) {
             // exchange gossip
             for (meeting in positions.values.filter { it.size > 1 }) {
                 println("${meeting.map { it.name }} meet...")
-                val allGossip = mutableSetOf<Gossip>()
-                for (d in meeting) allGossip.addAll(d.knowsAbout)
-
-                for (d in meeting) {
-                    d.haveYouHeardAboutThis(allGossip)
+                val allGossip = meeting.flatMap { it.knowsAbout }
+                meeting.forEach {
+                    it.haveYouHeardAboutThis(allGossip)
                 }
-            }
-
-
-            // move on
-            drivers.forEach {
-                it.drive()
             }
 
             if (everybodyKnowsEverything()) {
@@ -91,18 +69,17 @@ class Simulation(private val drivers: Collection<Driver>) {
                 return i+1
             }
 
+            // move on
+            drivers.forEach {
+                it.drive()
+            }
         }
         println("Day over. Not everybody is informed well...")
         return -1
     }
 
-    private fun everybodyKnowsEverything(): Boolean {
-        for (d in drivers) {
-            if (d.knowsAbout != everyGossipInTheWorld)
-                return false
-        }
-        return true
-    }
+    private fun everybodyKnowsEverything() =
+            drivers.all { it.knowsAbout.size == drivers.size }
 
 }
 
